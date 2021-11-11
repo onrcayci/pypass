5fa8658f758e1f2cebff701d0b2a11b647692667
Onur Cayci <onur.cayci@outlook.com>
2021-11-11 16:08:11 -0500

    Implement PasswordGenerator class

---
+++ pypass/passgen/passgen.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from hashlib import blake2b
+from os import urandom
+
+
+@dataclass(frozen=True)
+class PasswordGenerator:
+
+    def generate(self, length: int = 10, **constraints: bool) -> str:
+        # Generate salt for password generation
+        salt: bytes = urandom(blake2b.SALT_SIZE)
+        # Generate random number that will be used for password generation
+        data: bytes = urandom(length)
+        # Create the hash instance
+        generator = blake2b(data, salt=salt, digest_size=length)
+        password: str = generator.digest().decode(encoding="utf-8")
+        return password
