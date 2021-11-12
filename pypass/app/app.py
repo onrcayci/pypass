d179e6b30589657bea9ce5711c4ab093996dd03e
Onur Cayci <onur.cayci@outlook.com>
2021-11-12 14:13:11 -0500

    Update the structure of the repository to enable modular package builds

--- pypass/app/app.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-
-from pypass.passgen.routes import passgen_router
-
-app = FastAPI(title="PyPass")
-app.include_router(passgen_router, prefix="/api")
--- tests/test_pypass.py
+++
@@ -1,5 +0,0 @@
-from pypass import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
