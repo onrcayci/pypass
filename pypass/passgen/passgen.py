7d5cb18a5496881a2b08ecca41e9dba1f62afcb4
Onur Cayci <onur.cayci@outlook.com>
2021-11-11 19:28:35 -0500

    Update the condition for adding a special character; Document the password generator code by adding comments

--- pypass/passgen/passgen.py
+++ pypass/passgen/passgen.py
@@ -6,28 +6,60 @@ import random
 
 @dataclass(frozen=True)
 class PasswordGenerator:
+    """A class to generate secure passwords."""
 
     def generate(self, length: int = 10, **constraints: bool) -> str:
+
         # Generate salt for password generation
         salt: bytes = urandom(blake2b.SALT_SIZE)
+
         # Generate random number that will be used for password generation
         data: bytes = urandom(length)
+
         # Create the hash instance
         generator = blake2b(data, salt=salt, digest_size=length // 2)
+
+        # Generate the password and convert it from bytes to string
         password: str = generator.hexdigest()
+
+        # Update the password string based on the additional constraints
+        # If uppercase is an additional constraint
         if constraints.get("uppercase"):
+
+            # infinite loop
             while True:
+
+                # get a random character from the string
                 index = random.randrange(0, len(password))
+
+                # if the character is an alphabetical character
                 if password[index].isalpha():
+
+                    # make the character uppercase and exit the infinite loop
                     password = password[0:index] + \
                         password[index].upper() + password[index+1:]
                     break
+
+        # if special characters is an additional constraint
         if constraints.get("special"):
+
+            # infinite loop
             while True:
+
+                # get a random character from the string
                 index = random.randrange(0, len(password))
-                if password[index].islower():
+
+                # if the character is a lowercase alphabetical character or a
+                # numerical character
+                if (password[index].isalpha() and password[index].islower()) \
+                        or password[index].isnumeric():
+
+                    # exchange the character with a valid special character and
+                    # exit the infinite loop
                     password = password[0:index] + \
                         random.choice([".", ",", "!", "@", "_"]
                                       ) + password[index+1:]
                     break
+
+        # return the final resulting password
         return password
